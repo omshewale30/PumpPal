47b183740e82d686b346b67dcabbf88d5078a112
omshewale <omshewale030@gmail.com>
2023-11-21 17:08:38 -0700

    table view partially complete, date tye changed, implement delete

---
+++ PumpPal/View/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  PumpPal
+//
+//  Created by Om Shewale on 11/21/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
